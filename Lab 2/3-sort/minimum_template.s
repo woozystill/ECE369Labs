7696723cbdcd76ef50ab43a5e9fc7e57726efd7e
zscheve-c <zscheve@arizona.edu>
2026-09-15 16:59:29 -0500

    update

--- Lab 2/3-sort/minimum_template.s
+++ Lab 2/3-sort/minimum_template.s
@@ -1,8 +1,7 @@
 # Exercise 3
 # Max Score: 12 points
 #
-# Students: Zach Scheve - 50%
-# Huda Madi - 50%
+# Students: # Huda Madi - 100%
 #
 # minimum.s 
 # Finds the index of the smallest element in an integer array
